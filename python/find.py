22af41aee8dde86381e0ec3ad3773e6415c98503
ablenmilla <ablenefikre@gmail.com>
2025-03-23 19:42:24 +0300

    add 3 new string method practice files

---
+++ python/find.py
@@ -0,0 +1,4 @@
+text = "Hello world"
+print(text.find("world"))  # Output: 6
+print(text.find("Python"))  # Output: -1
+print(text.find("o", 5))  # Output: 7 (search starts from index 5)
---
+++ python/swapcase.py
@@ -0,0 +1,2 @@
+text = "Hello WoRLd"
+print(text.swapcase())  # Output: "hELLO wOrlD"
